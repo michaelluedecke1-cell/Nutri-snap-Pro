cb31122208ce066a1192bf64a3601860e7d00964
michaelluedecke1-cell <michael.luedecke1@googlemail.com>
2026-07-29 11:47:30 +0200

    Update cache name to nutrisnap-cache-v15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nutrisnap-cache-v14.9';
+const CACHE_NAME = 'nutrisnap-cache-v15';
 
 // Kritische Ressourcen, die sofort offline verfügbar sein müssen
 const PRECACHE_ASSETS = [
